927aaccaeb6f116dd22acf3afe3115f8b16551cd
Sahana Koli <sahanasultanakoli@gmail.com>
2023-12-21 14:02:20 +0600

    update .env file

--- index.js
+++ index.js
@@ -209,7 +209,6 @@ async function run() {
     });
 
 
-    
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -219,7 +218,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 
 app.get('/', (req,res) =>{
